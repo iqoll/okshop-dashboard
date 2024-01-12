552f39dee938ae52056874adaf09edfe016152c7
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2024-01-13 06:15:22 +0700

    sidebar bottom part

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,7 +1,13 @@
 import { FcBullish } from 'react-icons/fc'
-import { DASHBOARD_SIDEBAR_LINKS } from '../../lib/constants/Navigation'
+import { HiOutlineLogout } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
 import classNames from 'classnames'
+
+// Custom lib
+import {
+	DASHBOARD_SIDEBAR_BOTTOM_LINKS,
+	DASHBOARD_SIDEBAR_LINKS,
+} from '../../lib/constants/Navigation'
 
 const linkClasses =
 	'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
@@ -9,16 +15,30 @@ const linkClasses =
 export default function Sidebar() {
 	return (
 		<div className='flex flex-col bg-neutral-900 w-60 p-3 text-white'>
+			{/* Logo */}
 			<div className='flex items-center gap-2 px-1 py-3'>
 				<FcBullish fontSize={24} />
 				<span className='text-neutral-100 text-lg'>OkShop</span>
 			</div>
+			{/* Main Links */}
 			<div className='flex flex-col flex-1 py-8 gap-0.5'>
 				{DASHBOARD_SIDEBAR_LINKS.map((item) => (
 					<SidebarLink key={item.key} item={item} />
 				))}
 			</div>
-			<div>bottom part</div>
+			{/* Bottom Links */}
+			<div className='flex flex-col gap-0.5 pt-2 border-t border-neutral-700'>
+				{DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item) => (
+					<SidebarLink key={item.key} item={item} />
+				))}
+			</div>
+			{/* Logout */}
+			<div className={classNames('text-red-500 cursor-pointer', linkClasses)}>
+				<span className='text-xl'>
+					<HiOutlineLogout />
+				</span>
+				Logout
+			</div>
 		</div>
 	)
 }
