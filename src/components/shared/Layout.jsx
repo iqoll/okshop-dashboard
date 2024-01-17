dc7a5a7ca596e78086ca9bb563c262119313befb
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2024-01-17 09:15:13 +0700

    make the sidebar and header sticky

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -4,10 +4,14 @@ import Header from './Header'
 
 export default function Layout() {
 	return (
-		<div className='flex bg-neutral-100 h-screen w-screen overflow-hidden'>
-			<Sidebar />
-			<div className='flex-1'>
-				<Header />
+		<div className='relative flex bg-neutral-100 h-screen w-screen overflow-x-hidden'>
+			<div className='sticky flex top-0 left-0'>
+				<Sidebar />
+			</div>
+			<div className='relative flex-1'>
+				<div className='sticky top-0 left-0 z-10'>
+					<Header />
+				</div>
 				<div className='p-4'>{<Outlet />}</div>
 			</div>
 		</div>
